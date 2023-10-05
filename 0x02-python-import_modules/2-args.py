270c6bfff90e1ee04975d063946e87542d1e928d
Emran <ouakouriimran@gmail.com>
2023-10-05 07:04:23 -0700

    all args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from sys import argv
-   print('{} {}'.fomat(len(argv), if len(argv) = 1: print('argument') else: print('arguments')))
+    from sys import argv
+    n = len(argv)
+    print('{} {}{}'.format(n - 1, "argument" if n == 2
+                           else "arguments", "." if n == 1 else ":"))
+    for i in range(1, n):
+        print('{}: {}'.format(i, argv[i]))
