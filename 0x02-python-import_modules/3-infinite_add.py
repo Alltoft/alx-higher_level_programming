9c742d2d57a4c2369ff6c94883633a24544f7ecf
Emran <ouakouriimran@gmail.com>
2023-10-05 07:43:08 -0700

    counting calc

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
+    n = len(argv)
     i = 1
     a = 0
-    while i < l:
+    while i < n:
         a += int(argv[i])
         i += 1
-    if l == 0:
+    if n == 0:
         print(0)
     else:
         print(a)
