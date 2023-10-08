b6b4c5d3cc54c57e911af2f6392e1588cb15160f
Alltoft <ouakouriimran@gmail.com>
2023-10-08 19:39:32 +0100

    old

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    Old_tuple = tuple_a + (0, 0)
-    Older_tuple = tuple_b + (0, 0)
-    resault_tuple = (Old_tuple[0] + Older_tuple[0], Older_tuple[1] + Old_tuple[1])
-    return resault_tuple
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    c = (a[0] + b[0], b[1] + a[1])
+    return c
