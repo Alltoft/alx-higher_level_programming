068d1679c8be246c4af35670a67a0aced6bcec45
Alltoft <ouakouriimran@gmail.com>
2023-10-11 19:26:43 +0100

    lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     result = matrix.copy()
     for i in range(len(matrix)):
-        result[i] = list(map(lambda h: h ** 2, matrix[1]))
+        result[i] = list(map(lambda h: h ** 2, matrix[i]))
     return result
