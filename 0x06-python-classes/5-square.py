b1925e865912e2d1c7d951187915eaef2184ee09
Emran <ouakouriimran@gmail.com>
2023-10-24 20:13:17 +0100

    printing

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''a class Square that defines a square by size and area
+with property'''
+
+
+class Square:
+    '''defining sizes and area with handleing
+    the errors with property'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        self.Sarea = self.__size * self.__size
+        return self.Sarea
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for a in range(self.__size):
+                if self.__size != 0:
+                    print("#", end="")
+                else:
+                    print()
+            print()
