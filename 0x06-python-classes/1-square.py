e35d46cc86f30e364c80ef7d20c3c63dc66a0cd5
Emran <ouakouriimran@gmail.com>
2023-10-24 17:13:29 +0100

    setting size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''sitting a class square size'''
+
+
+class Square:
+    '''defining the size'''
+    __size = None
+
+    def __init__(self, new_size):
+        self.__size = new_size
