2ee2306f5ca3572abae67a1078f8f2f93079268d
Emran <ouakouriimran@gmail.com>
2023-10-28 18:40:55 +0100

    idk where am I stepping

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""this function adds two integers a and b
+if one of the integers is a float it turn it to integer
+if one of the integers is not int it raises a TypeError
+b is set to 98 by default
+Returns the sum of the two integers"""
+
+
 def add_integer(a, b=98):
+    """chekcs if there was an Error with the integers
+    it raises a Type Error or it changes it to an integer
+    otherwise it returns the sum"""
     if isinstance(b, float):
         b = int(b)
     if isinstance(a, float):
