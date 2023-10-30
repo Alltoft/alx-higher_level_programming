e493b7869e4318a1b3059b26ac8c8e2a66acc729
Emran <ouakouriimran@gmail.com>
2023-10-30 17:27:52 +0100

    now its correct! .. I think

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):"""
     def __init__(self, width=0, height=0):
         self.__width = width
-        self.__height= height
+        self.__height = height
 
     @property
     def width(self):
