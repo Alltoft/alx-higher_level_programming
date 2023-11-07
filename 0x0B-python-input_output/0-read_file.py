943ea949db548d9d1a75456a0a5625d28cf6fc94
Emran <ouakouriimran@gmail.com>
2023-11-07 15:58:14 +0100

    with open

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""a function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """Prototype: def read_file(filename=""):
+must use the with statement
+dont need to manage file permission or file doesn't exist exceptions.
+are not allowed to import any module"""
     with open(filename, encoding="utf-8") as f:
         print(f.read())
