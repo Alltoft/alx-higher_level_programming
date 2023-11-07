c6784b31db02effb7f784ded5a7656dca8f9badc
Emran <ouakouriimran@gmail.com>
2023-11-07 17:54:06 +0100

    we are in jsons house

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a function that creates an Object from a “JSON file”"""
+import json
+
+
+def load_from_json_file(filename):
+    """Prototype: def load_from_json_file(filename):
+You must use the with statement
+You dont need to manage exceptions if the JSON string doesnt
+represent an object.
+You dont need to manage file permissions / exceptions."""
+
+    with open(filename, "w") as f:
+        return (json.load(f))
