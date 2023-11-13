aeffd4608b92e8e093ce95e3252ebbbe4d726ed7
Emran <ouakouriimran@gmail.com>
2023-11-13 16:44:37 +0100

    all files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,40 +17,40 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """a static method that returns the JSON string representation of
-    list_dictionaries"""
+        list_dictionaries"""
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    """a class method that writes the JSON string
-    representation of list_objs to a file"""
     @classmethod
     def save_to_file(cls, list_objs):
+        """a class method that writes the JSON string
+        representation of list_objs to a file"""
         conv = []
         for i in list_objs:
             conv.append(cls.to_dictionary(i))
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(conv))
 
-    """a static method that returns the list of the
-    JSON string representation json_string"""
     @staticmethod
     def from_json_string(json_string):
+        """a static method that returns the list of the
+        JSON string representation json_string"""
         if not json_string:
             return []
         return json.loads(json_string)
 
-    """a  class method that returns an instance
-    with all attributes already set (using a "dummy")"""
     @classmethod
     def create(cls, **dictionary):
+        """a  class method that returns an instance
+        with all attributes already set (using a "dummy")"""
         dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy
 
-    """a class method that returns a list of instances"""
     @classmethod
     def load_from_file(cls):
+        """a class method that returns a list of instances"""
         try:
             with open(cls.__name__ + ".json", "r") as f:
                 file_data = f.read()
