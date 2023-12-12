d758ec339190146495f5119cce92a35d1d3e4bea
Emran <ouakouriimran@gmail.com>
2023-12-12 22:32:49 +0100

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
-INSERT INTO second_table VALUES (89, "John", 10);
-INSERT INTO second_table VALUES (2, "Alex", 3);
-INSERT INTO second_table VALUES (3, "Bob", 14);
-INSERT INTO second_table VALUES (4, "George", 8);
+INSERT INTO second_table values (1, "John", 10);
+INSERT INTO second_table values (2, "Alex", 3);
+INSERT INTO second_table values (3, "Bob", 14);
+INSERT INTO second_table values (4, "George", 8);
