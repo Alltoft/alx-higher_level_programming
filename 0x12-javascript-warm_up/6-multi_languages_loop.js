184d96f0a8501c108c8ada0fe0d4afd502b557d4
Emran <ouakouriimran@gmail.com>
2024-01-08 17:52:20 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const linesPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let number = 0;
+while (number < 3) {
+  console.log(linesPrint[number]);
+  number++;
+}
