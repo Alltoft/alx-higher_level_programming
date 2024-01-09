e56e818dc1063fe5a3bf8467d0f2242b07a5d8bb
Emran <ouakouriimran@gmail.com>
2024-01-09 18:44:45 +0100

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let temp = 0, retNumber = 0;
-    while (temp < list.length) {
-       if (list[temp] === searchElement) {
-        retNumber++;
-       }
-       temp++;
+  let temp = 0; let retNumber = 0;
+  while (temp < list.length) {
+    if (list[temp] === searchElement) {
+      retNumber++;
     }
-    return(retNumber)
-}
+    temp++;
+  }
+  return (retNumber);
+};
