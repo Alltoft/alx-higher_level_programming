88bdffb950fbe857b2c5cbc45eb6308cb339ddf3
Emran <ouakouriimran@gmail.com>
2024-01-09 20:49:39 +0100

    9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let countNumber = 0;
 exports.logMe = function (item) {
-  countNumber++;
-  console.log(countNumber + ': ' + item);
+    console.log(countNumber + ': ' + item);
+    countNumber++;
 };
