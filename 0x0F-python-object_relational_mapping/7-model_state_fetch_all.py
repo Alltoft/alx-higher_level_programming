e3e7d817e19672871e4c61aeb175080e7fb6e204
Emran <ouakouriimran@gmail.com>
2024-01-12 22:26:38 +0100

    7-model_state_fetch_all.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""
-Define a State class
-that inherits from sqlalchemy Base
-"""
-from sqlalchemy import create_engine, Column, ForeignKey, String, Integer, CHAR
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+"""Start link class to table in database"""
+
 import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    # A session is like a workspace for making changes and
-    # interacting with the database.
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state_names = session.query(State).order_by(State.id)
-
-    for state in state_names:
-        print('{}: {}'.format(state.id, state.name))
+    result = session.query(State).order_by(State.id)
+    for r in result:
+        print('{}: {}'.format(r.id, r.name))
