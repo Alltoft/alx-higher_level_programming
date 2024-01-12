9993d008bd7891be3597a276aed545a73afed966
Emran <ouakouriimran@gmail.com>
2024-01-12 22:22:31 +0100

    7-model_state_fetch_all.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
-
-from model_state import Base, State
+"""
+Define a State class
+that inherits from sqlalchemy Base
+"""
+from sqlalchemy import create_engine, Column, ForeignKey, String, Integer, CHAR
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format('alo', 'Emran', 'hbtn_0e_6_usa'))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
+    # A session is like a workspace for making changes and
+    # interacting with the database.
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id)
-    for r in result:
-        print('{}: {}'.format(r.id, r.name))
+
+    state_names = session.query(State).order_by(State.id)
+
+    for state in state_names:
+        print('{}: {}'.format(state.id, state.name))
