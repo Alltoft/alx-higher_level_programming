55be19e4ba738d7a8454cdaf6f32adeea09e4273
Emran <ouakouriimran@gmail.com>
2024-01-26 23:15:24 +0100

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+"""a Python script that takes your GitHub credentials (username and
+password) and uses the GitHub API to display your id"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    bred = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    reques = requests.get('https://api.github.com/user', auth=bred)
+    print(reques.json().get("id"))
