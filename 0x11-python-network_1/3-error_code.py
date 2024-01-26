60e57b0515876b4d036a5649e3caaa24e16db44e
Emran <ouakouriimran@gmail.com>
2024-01-26 19:58:21 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,6 @@
 """a Python script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8)"""
 import sys
-from urllib import parse
 from urllib import request
 
 
