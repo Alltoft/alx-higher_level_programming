2419204d412daa6c620254cdfcb138e891ccbd50
Emran <ouakouriimran@gmail.com>
2024-01-26 18:01:50 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ a Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+from urllib import request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         disp = resp.read()
         print(('Body response:\n\t- type: {}\n\t- content: {}\n\t\
 - utf8 content: {}').format(type(disp), disp, disp.decode('utf-8')))
