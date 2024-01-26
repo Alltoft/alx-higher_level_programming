72cd0d8c4ff5397e1ecff658c19ef0cda77be3b2
Emran <ouakouriimran@gmail.com>
2024-01-26 20:36:54 +0100

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body of
+the response (decoded in utf-8)"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    dict = {'email': sys.argv[2]}
+    data = requests.post(dict).encoding('utf-8')
+    req = requests.request(url, data)
+    resp = requests.get(req)
+    rd = resp.text
+    print(rd)
