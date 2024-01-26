81549519c536906184bb620be8e08fdc91fa32e2
Emran <ouakouriimran@gmail.com>
2024-01-26 20:51:53 +0100

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        resp = requests.get(url)
+        text = resp.text()
+        print(text)
+    except Exception as error:
+        print('Error code: {}'.format(error.code))
