abb8c0997847b1718e99f084c4b339c9e64c6f74
Emran <ouakouriimran@gmail.com>
2024-01-26 17:51:57 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         disp = resp.read()
         print(('Body response:\n\t- type: {}\n\t- content: {}\n\t\
-    - utf8 content: {}').format(type(disp), disp, disp.decode('utf-8')))
+- utf8 content: {}').format(type(disp), disp, disp.decode('utf-8')))
