eaf99bff86b761db339325b534e51e93e5a4664a
Emran <ouakouriimran@gmail.com>
2024-02-20 19:35:51 +0100

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const Arguments = process.argv;
+const IDs = 8; let temp = 1; let EpNum = 0; let vari = 0;
+while (temp < IDs) {
+  request(Arguments[2] + '/' + temp, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const toJson = JSON.parse(body);
+      const charactr = toJson.characters;
+      for (const url of charactr) {
+        if (url.endsWith('18/')) {
+          vari++;
+        }
+        EpNum = vari;
+      }
+    }
+  });
+  temp++;
+}
+setTimeout(() => {
+  console.log(vari);
+}, 2000);
