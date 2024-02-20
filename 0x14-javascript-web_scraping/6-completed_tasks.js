275c32b94a73f9e32366904e879eee0e309120ee
Emran <ouakouriimran@gmail.com>
2024-02-20 23:13:55 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let data; let temp = 1; let id = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    data = JSON.parse(body);
+    const result = {};
+    data.forEach(function (IdNum){
+        IdNum.userId
+        id++;
+    });
+    while (temp <= id){
+        data.forEach(function (TaskComp) {
+        if (TaskComp.userId === temp) {
+            if (TaskComp.completed === true) {
+                if (result[temp]) {
+                    result[temp] += 1;
+                } else {
+                    result[temp] = 1;
+                }
+            }
+        }
+        });
+        temp++;
+    }
+    console.log(result);
+}
+});
